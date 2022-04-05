a884163ca4684f5146dd8ebc5554fd9daa45793d
Devin Coughlin <coughlindevin@gmail.com>
2022-04-05 19:06:09 -0400

    updated some comments

--- tests.py
+++ tests.py
@@ -4,9 +4,7 @@ def main():
     time_tests()
 
 def time_tests():
-    # NOTE finished n=6,k=4 in 13 seconds: no shatterable set
-    #powerset_shatter = shatter(5,4)
-    # NOTE combination_search(19,12,8) executed in 325_964_517_900ns ~5m:26s
+    # NOTE column_search(19,12,8) executed in 325_964_517_900ns ~5m:26s on Devin's laptop
     shatter = column_search(6,3,3)
 
 if __name__ == "__main__":
