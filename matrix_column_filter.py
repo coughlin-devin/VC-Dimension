3b229aac2fc5f292b6047d341fc15f048778e749
Devin Coughlin <coughlindevin@gmail.com>
2022-04-07 20:34:19 -0400

    optimized for loops using numpy functions

--- matrix_column_filter.py
+++ matrix_column_filter.py
@@ -3,7 +3,6 @@ import time
 from random import sample
 from communication_matrix import S
 from itertools import combinations
-from tqdm import tqdm
 
 #%% Decorator for timing function execution time
 def timer(func):
@@ -23,13 +22,13 @@ def print_shatter(iterable,n):
         shatter.append(bin(i)[2:].zfill(n))
     return shatter
 
+# OPTIMIZE? should try to break apart massive dictionary copmprehension to make code readable
 # NOTE: returns a set of unique communication matrix column numbers whose column sums are within the given range
 def filter_cmatrix(matrix, lower, upper):
     return set({tuple(matrix[:,i]) : i for i,sum in enumerate(np.sum(matrix, axis=0)) if sum <= upper and sum >= lower}.values())
     # [i for i,sum in enumerate(np.sum(matrix, axis=0)) if sum <= upper and sum >= lower]
 
 #IDEA look for combinations with close to 50/50 1 to 0 populated bits
-#IDEA remove duplicate values with identical columns
 # arguments: n = binary sequence length
 #            k = binary subsequence length
 #            vc = size of set attempting to shatter (vc dimension)
@@ -38,49 +37,34 @@ def filter_cmatrix(matrix, lower, upper):
 # behavior: search all combinations of size vc from subset of binary sequences length n with communication matrix column sums in range s-l <= sum <= s+u
 # returns: shattered set if found, null otherwise
 @timer
-def column_search(n,k,vc,l,u):
+def shatter(n,k,vc,l,u):
     # communication matrix
     cm = S(n,k) # OPTIMIZE make creating communication matrix faster
-    # shatterable set
-    shatter = set()
-    # set of tuples of values from communication matrix
-    comm_set = set()
     # IDEA use combinations whose column sums are in a specific range of values: [comb for comb in combinations(filter_cmatrix(cm, l, u), 2) if sum([col_sums[num] for num in comb]) < 3] # < 3 is arbitrary condition
-    # NOTE for each combinations of size s with column sums in range 2**(k-1) - 0 <= sum <= 2**(k-1) + 0
-    possible_shattered_sets = list(combinations(filter_cmatrix(cm, l, u), vc))
-    bar = tqdm(possible_shattered_sets, colour="green")
-    for comb in bar:
-        # clear comm_set
-        comm_set.clear()
-        # for each subsequence
-        for i in range(2**k):
-            comm_tuple = tuple()
-            for j in comb:
-                # add the value from the communication matrix to a tuple
-                comm_tuple = comm_tuple + (cm[i][j],)
-            # add comm tuple to a set
-            comm_set.add(comm_tuple)
-        if len(comm_set) == 2**vc:
-            shatter = comb
-            bar.close()
-            print(f"Shattered set: {print_shatter(shatter,n) }, n={n}, k={k}, vc={vc}, l={l}, u={u}")
-            return shatter
+    # NOTE for each combination of size s with column sums in range 2**(k-1) - 0 <= sum <= 2**(k-1) + 0
+    for comb in combinations(filter_cmatrix(cm, l, u), vc):
+        # get an array of arrays by taking elements from the communication matrix with indeces of comb from each column
+        array = np.take(cm, comb, axis=1)
+        # remove duplicate arrays by converting array to a set of tuples
+        array = set(map(tuple, array))
+        if len(array) == 2**vc:
+            print(f"Shattered set: {print_shatter(comb,n)}, n={n}, k={k}, vc={vc}, l={l}, u={u}")
+            return comb
 
 # random sample on combinations of values with specific communication matrix column sums
-def random_column_search(n,k,s):
+def random_column_search(n,k,vc,l,u):
     cm = S(n,k)
-    shatter = set()
     comm_set = set()
     # while have not found a shattered set
-    while not shatter:
-        rset = sample(filter_cmatrix(cm, 2**(k-1)), s) # IDEA mess around with this range, maybe first/second half or middle half of the range is enough because of symmetry?
+    while 1:
+        rset = sample(filter_cmatrix(cm, l, u), vc) # IDEA mess around with this range, maybe first/second half or middle half of the range is enough because of symmetry?
+        # NOTE: double for loop exact same code as in column_search. Maybe pull out into it's own function?
         comm_set.clear()
         for i in range(2**k):
             comm_tuple = tuple()
             for j in rset:
                 comm_tuple = comm_tuple + (cm[i][j],)
             comm_set.add(comm_tuple)
-        if len(comm_set) == 2**s:
-            shatter = rset
-            print(f"Shatterable set: {print_shatter(shatter,n)}")
-            return shatter
+        if len(comm_set) == 2**vc:
+            print(f"Shattered set: {print_shatter(rset,n)}, n={n}, k={k}, vc={vc}, l={l}, u={u}")
+            return rset
--- tests.py
+++ tests.py
@@ -1,13 +1,14 @@
-from matrix_column_filter import column_search, random_column_search
+from matrix_column_filter import shatter, random_column_search
 
 def main():
-    n = 10
-    k = 5
+    n = 9
+    k = 6
     vc = 5
-    l = 2**(vc-1) # NOTE: 2^(S-1)<=column sum<=2^k-2^(S-1)
-    u = 2**k - 2**(vc-1)
-    shatter = column_search(n,k,vc,l,u)
-    print(shatter)
+    l = 30 #2**(vc-1) # NOTE: 2^(S-1)<=column sum<=2^k-2^(S-1)
+    u = 34 #2**k - 2**(vc-1)
+    #shatter = random_column_search(n,k,vc,l,u)
+    s = shatter(n,k,vc,l,u)
+    print(s)
 
 if __name__ == "__main__":
     main()
